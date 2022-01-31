68774fbcd4191f5b869e600be934cb5012edeb2f
SO-WAT <nishruth9@gmail.com>
2022-01-31 10:38:29 +0300

    another commit

---
+++ operators.php
@@ -0,0 +1,73 @@
+<?php
+//Arithmetic Operators
+$var1=98;
+$var2=43;
+echo $var1+$var2;
+echo"<br>";
+echo $var1-$var2;
+echo"<br>";
+echo $var1*$var2;
+echo"<br>";
+echo $var1/$var2;
+echo"<br>";
+echo $var1%$var2;
+echo"<br>";
+
+echo"<br>";
+//Assignment Operators
+$no3=789;
+echo $no3;
+
+echo"<br>";
+//Comparison operator
+
+$var4=564;
+$var5="564";
+var_dump($var4==$var5);//equals operator
+echo"<br>";
+
+var_dump($var4===$var5);//identical operator
+
+echo"<br>";
+var_dump($var4!=$var5);
+echo"<br>";
+
+var_dump($var4||$var5);
+echo"<br>";
+$var6=895;
+$var7=456;
+
+var_dump($var6<$var5);//less than
+echo"<br>";
+var_dump($var7>$var6);//greater than
+echo"<br>";
+
+var_dump($var4>=$var7);
+echo"<br>";
+var_dump($var7<=$var5);
+echo"<br>";
+var_dump($var6<=>$var7);
+echo"<br>";
+
+//logical operators
+echo $var6++;
+echo"<br>";
+echo ++$var7;
+
+echo"<br>";
+$var0=6;
+$var01=2;
+echo"<br>";
+echo --$var0;
+echo"<br>";
+echo $var01--;
+echo"<br>";
+var_dump($var0&&$var01);
+
+
+
+
+
+
+
+
