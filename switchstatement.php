38d2f61f6c9021358721411358eb7664288482f7
SO-WAT <nishruth9@gmail.com>
2022-02-02 08:37:11 +0300

    MY Website

--- switchstatement.php
+++ switchstatement.php
@@ -5,6 +5,17 @@ switch ($myfruit){
 
     case"Oranges":
         echo "I have some sweet oranges";
+        break;
+    case "Water Melon":
+        echo"I find them very waterly";
+        break;
+    case "Bananas":
+        echo"I dislike bananas";
+        break;
+        case "Mangoes";
+        echo"Mangoes are very succulent";
+        break;
+
 
 
 
