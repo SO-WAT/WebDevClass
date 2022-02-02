c24dcfb26ec4987491538d91ba1f8d6f7e5c94ab
SO-WAT <nishruth9@gmail.com>
2022-02-02 08:21:42 +0300

    My Website

--- conditional.php
+++ conditional.php
@@ -1,1 +1,29 @@
 <?php
+//if statements
+$var=76;
+if($var>67){
+    echo "How is your morning?";
+}
+echo "<br>";
+
+//if else statement
+$var1=43;
+$var2=98;
+
+if ($var1>=$var2 && $var=76){
+    echo"This is true";
+    }else {
+    echo "This is not true";
+}
+echo"<br>";
+//if ....else if....
+$marks=87;
+if ($marks<87){
+    echo"Make some improvements";
+}else if($marks<87 and $marks<=20){
+    echo"You are a confused fellow";
+}else if($marks<=60 && $marks>70){
+    echo"Fair enough";
+}else {
+    echo "You really outdid yourself here";
+}
