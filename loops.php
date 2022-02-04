b241bd64d421f77a3ff27e12cc76f8c536c2d422
SO-WAT <nishruth9@gmail.com>
2022-02-04 09:45:36 +0300

    another

--- loops.php
+++ loops.php
@@ -23,6 +23,9 @@ do{
 
 echo"<br>";
 echo"#############################while loop##########################################";
+
+echo"<br>";
+echo"<h1>While Loop Class Assignment</h1>";
 echo"<br>";
 $var=20; //my loop starts here
     while($var<=500){  //this is the limit
@@ -33,8 +36,33 @@ $var=20; //my loop starts here
 echo"<br>";
     echo"........................................do.....while loop..............................................";
     echo"<br>";
-    $var2=3;
+    echo"<h1>Do.....While Class Assignment</h1>";
+    $var2=3; //initial counter
 do{
     echo"My number is : $var2<br>";
-    $var2+=2;
-}while($var2<=30);
+    $var2+=2;// increament counter
+}while($var2<=30); //test counter
+
+
+echo"<br>";// //include the 3 counters inside
+echo",,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,FOR LOOP,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,";
+echo"<br>";
+
+for ($nambari=-2;$nambari<=10;$nambari+=2){
+    echo"Now you have achieved $nambari<br>";
+
+}
+
+echo"<br>";
+for ($namb=10;$namb>=-2;$namb-=2){
+    echo"Well, this looks like it $namb<br>";
+
+}
+echo"<br>";
+
+echo ",,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,foreach loop********************************************************";
+echo"<br>";
+$colors=array("red","green","blue","yellow");
+foreach($colors as $value){
+    echo "$value <br>";
+}
